f10c288b4578071940d7fe667b3600f48b5df3e3
choie1346 <choie1346@gmail.com>
2026-03-24 17:10:57 +0900

    Dynamic Memory-1

--- 메인.cpp
+++ 메인.cpp
@@ -1,67 +1,60 @@
 //----------------------------------------------
 // 2026 1학기 STL 월56 화 78      2026.03.10(화)
 //----------------------------------------------
-// 
+// 동적 메모리 할당(Dynamic memory allocation)
+// Memory Allocation
+// 정적(Static) - Program 할 때 사용할 메모리를 결정할 수 있는 경우    Compile-time
+// 동적(Dynamic) - 실행한 이후에 결정되는 메모리(크기, 주소가 결정됨)   Run-time
+// 동적은 실행 이후에 결정되기 때문에 이름을 지어줄 수 없음.
+// 이름은 곧 주소다.
 //----------------------------------------------
 #include <iostream>
-#include <print>
-#include <string>
-#include <array>
-#include <fstream>
+#include <numeric>
 #include "save.h"
 using namespace std;
 
-class Dog {
-
-private:
-    string name;        // [10, 30] 사의의 소문자로 구성된 이름
-    size_t id;          // [0, 9999]
-
-
-
-    friend ostream& operator<<(ostream& os, const Dog& dog) {
-        print(os, "[{:4}] - {}", dog.id, dog.name);
-        return os;
-    }
-
-    friend istream& operator>>(istream& is, Dog& dog) {
-        is.read((char*)&dog, sizeof(Dog));
-        return is;
-    }
-};
-
-
-// SSO(Small String Optimization) - 성능 최적화를 위해 짧은 문자열을 free-store가 아닌 stack에 저장하는 것
-
-
-// [문제] 파일 "Dog천마리"에는 class Dog객체 1000개가 저장되어 있다.
-// 파일은 binary mode이고 각 객체는 메모리 크기 그대로 stream의 write함수로 기록하였다.
-// 모든 객체를 한번의 write함수를 사용하여 기록하였다.
-// Dog의 멤버는 위의 코드와 같다.
-// 메모리에 모두 읽어오시오.
-// 메모리에 읽은 Dog를 화면에 모두 출력하라.
+// [문제] 사용자가 입력한 수만큼 int를 저장할 메모리를 확보하라.
+// 1부터 시작하는 정수로 메모리를 채워라
+// 합계를 화면에 출력하시오
 
 //--------
 int main()
 //--------
 {
-    ifstream in{ "Dog천마리", ios::binary };
-    if (not in) {
-        cout << "파일을 열 수 없습니다." << endl;
-        return 4444;
+    int* p;
+
+    while (true) {
+        size_t num;
+        cout << "입력 - ";
+        cin >> num;
+        
+        try {
+            p = new int[num];
+        }
+        catch (std::exception& e) {
+            cout << "메모리가 고갈 - " << e.what() << endl;
+        }
+        // 메모리 관리자에서 num만큼의 크기를 요청함.
+        // 메모리를 다 쓰면 -> 페이징함.
+        // 최근에 가장 안 쓴 메모리를 하드디스크에 저장하고 메모리를 줌.
+
+        iota(p, p + num, 1);    // p위치부터 p+num위치까지 1부터 채우기
+
+        //long long sum{};
+        //for (int i{}; i < num; ++i) {
+        //    p[i] = i + 1;
+        //    sum += p[i];
+        //}
+
+        long long sum = accumulate(p, p + num, 0LL); // static_cast<long long>(0)
+        // 시작위치, 마지막 위치, 시작할 값
+
+        cout << sum << endl;
+
+        delete[] p;
     }
 
-    array<Dog, 1000> dogs;
-
-    in.read((char*)dogs.data(), dogs.size() * sizeof(Dog));
-
-    for (const Dog& dog : dogs)
-        cout << dog << endl;
-    // 이렇게 하면 SSO때문에 복사가 제대로 안 됨.
-
-
+    // free - C
     
-    // text mode와 binary mode는 교차가 가능
-    // (text mode로 쓰기 후 binary mode로 읽기가 가능)
     // save("메인.cpp");
 }
