c042c96960624e7165f5afed9dda4af8671e9bcf
choie1346 <choie1346@gmail.com>
2026-03-31 17:19:59 +0900

    람다 함수의 typeid와 호출방

--- 메인.cpp
+++ 메인.cpp
@@ -6,26 +6,40 @@
 // 1. 함수
 //  - 함수 포인터도 호출 가능하다
 // 2. lambda
+// 3. () 연산자를 구현한 클래스 - 객체를 functor(함수객체)라 함.
+// 4. 멤버함수와 멤버함수 포인터
+// 
+// --> functinon 클래스로 일관된 사용가능
 //----------------------------------------------
 #include <iostream>
 #include "save.h"
 using namespace std;
 
-void f()
-{
-    cout << "난 호출 가능해요" << endl;
-}
+
 
 //--------
 int main()
 //--------
 {
-    void (*a)() = f;    // a는 함수 f를 가리키는 포인터
+    class Sakura {
+    public:
+        void operator()() {
+            cout << "안녕 난 람다야" << endl;
+        };
+    };
+    // 람다의 타입
+    // class `int __cdecl main(void)'::`2'::<lambda_1>
 
+    Sakura a;
     a();
 
-    // a는 STACK, f는 CODE segment에 존재함.
-    // 
+    cout << typeid(a).name() << endl;
+    // class를 지역에 정의했을 때 a의 타입
+    // class `int __cdecl main(void)'::`2'::Sakura
+
+
+    // class를 전역에 정의했을 때 a의 타입
+    // class Sakura
 
     // save("메인.cpp");
 }
