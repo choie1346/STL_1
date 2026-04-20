21962478977d034467233c11c72c3ff0859b75df
choie1346 <choie1346@gmail.com>
2026-04-20 15:18:25 +0900

    vector를 쓰지 않아도 된다.

--- 메인.cpp
+++ 메인.cpp
@@ -23,9 +23,8 @@ int main()
 //--------
 {
     // [문제] 키보드에서 입력한 모든 정수의 합계를 출력하라.
-    vector<int> v{ istream_iterator<int>{cin}, {} };
 
-    long long sum = accumulate(v.begin(), v.end(), 0LL);
+    long long sum = accumulate(istream_iterator<int>{cin}, {}, 0LL);
 
     cout << sum << endl;
 
